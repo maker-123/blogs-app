8ab8b68d5351698926b755e6dce601ee098ddf4b
alvin <=>
2026-02-04 13:55:12 +0800

    add font fortawesome

--- src/pages/blog/PostList.tsx
+++ src/pages/blog/PostList.tsx
@@ -3,6 +3,11 @@ import { supabase } from "../../services/supabaseClient";
 import CommentForm from "../../components/CommentsForm";
 import EditModal from "../../components/Editmodal";
 import { Post } from "../../types/blog.typs";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faComment } from "@fortawesome/free-regular-svg-icons";
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
+
 const PostList = () => {
   const [posts, setPosts] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
@@ -67,13 +72,20 @@ const PostList = () => {
         >
           Create New Post
         </button>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-y-4">
           {posts.map((post) => (
             <div
               key={post.id}
               className="w-full max-w-4xl px-6 py-12 lg:px-8 bg-white rounded-lg shadow-md bg-white/5 backdrop-blur-sm border border-white/10"
             >
-              <h1 className="text-lg font-semibold text-white">{post.title}</h1>
+              <div className="flex justify-between items-center">
+                <h1 className="text-lg font-semibold text-white">
+                  {post.title}
+                </h1>
+                <button className="p-2 hover:bg-gray-700  rounded-full transition">
+                  <FontAwesomeIcon icon={faEllipsis} color="white" size="lg" />
+                </button>
+              </div>
               {/*<p className="text-white">{post.body}</p>*/}
               {/*<p>{post.caption}</p>*/}
               <div className="border border-white/10 my-4 rounded-lg overflow-hidden">
@@ -83,24 +95,31 @@ const PostList = () => {
                   className="object-cover w-full "
                 />
               </div>
-              <button
-                onClick={() =>
-                  setActivePostId(activePostId === post.id ? null : post.id)
-                }
-                className="bg-gray-500 text-white px-2 py-1 rounded mr-2"
-              >
-                {activePostId === post.id ? "Close" : "Comment"}
-              </button>
-              {activePostId === post.id && (
-                <div className="">
-                  <CommentForm
-                    postId={post.id}
-                    onCommentAdded={() => {
-                      setActivePostId(null);
-                    }}
+              <div>
+                <button
+                  onClick={() =>
+                    setActivePostId(activePostId === post.id ? null : post.id)
+                  }
+                  className=" text-white px-2 py-1 rounded hover:bg-gray-700 transition  w-full"
+                >
+                  <FontAwesomeIcon
+                    icon={faComment}
+                    color="white"
+                    className="mr-2"
                   />
-                </div>
-              )}
+                  {activePostId === post.id ? "Close" : "Comment"}
+                </button>
+                {activePostId === post.id && (
+                  <div className="">
+                    <CommentForm
+                      postId={post.id}
+                      onCommentAdded={() => {
+                        setActivePostId(null);
+                      }}
+                    />
+                  </div>
+                )}
+              </div>
 
               <button
                 onClick={() => openEditModal(post)}
